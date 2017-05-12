5c558961670e12ce947a56a0b7c4bc27f1538f7e
loam <soeren@openio.io>
2017-05-12 15:24:18 +0900

    Add logs

--- server/main.go
+++ server/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"regexp"
@@ -22,14 +24,22 @@ var (
 	count                          int
 	titleRe                        *regexp.Regexp = regexp.MustCompile("\\[download]\\s+Destination:\\s+(.+)")
 	progressRe                     *regexp.Regexp = regexp.MustCompile("\\[download]\\s+(\\d+\\.\\d)%")
+	logFile, cmdLogFile            string
+	logger                         *log.Logger
+	cmdLogger                      *os.File
 )
 
 func init() {
 	flag.StringVar(&downloadDir, "dir", "", "Path to the download directory")
 	flag.StringVar(&archivePath, "archive", "", "Path to the archive file")
 	flag.StringVar(&icon, "icon", "", "Path to the icon")
+	flag.StringVar(&logFile, "log", "", "Log file")
+	flag.StringVar(&cmdLogFile, "cmdlog", "", "Command log file")
 
 	flag.Parse()
+
+	logger = log.New(os.Stdout, "", log.LstdFlags)
+	cmdLogger = os.Stdout
 }
 
 func ServeHTTP(rw http.ResponseWriter, req *http.Request) {
@@ -40,6 +50,24 @@ func ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	if logFile != "" {
+		f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
+		if err != nil {
+			logger.Fatalln(err)
+		}
+		defer f.Close()
+		logger = log.New(f, "", log.LstdFlags)
+	}
+
+	if cmdLogFile != "" {
+		f, err := os.OpenFile(cmdLogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
+		if err != nil {
+			logger.Fatalln(err)
+		}
+		defer f.Close()
+		cmdLogger = f
+	}
+
 	gtk.Init(nil)
 
 	go func() {
@@ -47,13 +75,15 @@ func main() {
 	}()
 
 	http.HandleFunc("/", ServeHTTP)
-	log.Println("Listening...")
+	logger.Println("Listening...")
 	if err := http.ListenAndServe(":12345", nil); err != nil {
-		log.Println(err)
+		logger.Fatalln(err)
 	}
 }
 
 func download(url string, playlist bool) {
+	logger.Println("Request", url, playlist)
+
 	notify("Download (playlist:", playlist, ")")
 
 	count++
@@ -94,10 +124,12 @@ func download(url string, playlist bool) {
 		"--output", filepath.Join(downloadDir, "%(title)s.%(ext)s"),
 		url)
 
-	out, err := cmd.StdoutPipe()
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		panic(err)
 	}
+
+	out := io.TeeReader(stdout, cmdLogger)
 
 	if err := cmd.Start(); err != nil {
 		panic(err)
@@ -124,6 +156,8 @@ func download(url string, playlist bool) {
 				title = wholeTitle.Slice(0, 30)
 			}
 
+			logger.Println("Title", wholeTitle)
+
 			continue
 		}
 
@@ -135,10 +169,12 @@ func download(url string, playlist bool) {
 	}
 
 	if err := cmd.Wait(); err != nil {
+		logger.Println("Failed", url, err)
 		notify(err)
 	}
 
-	notify("Done")
+	logger.Println("Done", title)
+	notify("Done ", title)
 
 	indicator.SetStatus(appindicator.StatusPassive)
 }
